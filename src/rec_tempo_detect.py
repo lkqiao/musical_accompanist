abb71e0660da802f36ff2acf099e3e5f88684128
luke qiao <LukeQiao2222@gmail.com>
2025-05-14 02:23:58 -0700

    added comments

--- src/rec_tempo_detect.py
+++ src/rec_tempo_detect.py
@@ -5,6 +5,7 @@ import librosa.display
 import soundfile as sf
 import os
 
+# parameters
 filename = 'minecraft.mp3'
 signal, sr = librosa.load(f'audio_in/{filename}')  
 
@@ -12,21 +13,30 @@ hop_length = 512
 win_dur = 30.0 
 win_length = int((win_dur*sr)/hop_length)
 
+# maybe try wavelet transform instead of stft for onset detection (find 
+# power of frequency spectrum of each frame and compute spectral flux) 
+# what if each basis function was a wavelet + sin wave?
 onset_env = librosa.onset.onset_strength(y=signal, sr=sr, hop_length=hop_length)
 if np.all(onset_env == 0): raise ValueError("Onset envelope is all zeros. Try a different audio file.")
 
+# does autocorrelation of the onset envelope to get the tempogram
 tempogram = librosa.feature.tempogram(onset_envelope=onset_env, sr=sr,
                                       hop_length=hop_length, win_length=win_length)
+
+# get rid of first row, which is the zero lag
 tempogram = tempogram[1:,:]
+
+# get the tempos and times from tempogram
 tempos = librosa.tempo_frequencies(tempogram.shape[0], sr=sr, hop_length=hop_length)
 times = librosa.frames_to_time(np.arange(tempogram.shape[1]), sr=sr, hop_length=hop_length)
 
+# limit tempogram to a range of tempos
 min_bpm, max_bpm = 30, 200
-
 mask = (tempos >= min_bpm) & (tempos <= max_bpm)
 tempogram = tempogram[mask,:]
 tempos = tempos[mask] 
 
+# plot the tempogram
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(tempogram, sr=sr, hop_length=hop_length,
                          x_axis='time', y_axis='tempo', y_coords=tempos)
@@ -36,9 +46,11 @@ plt.colorbar(label='Autocorrelation')
 plt.tight_layout()
 plt.show()
 
+# find the dominant tempo at each time frame
 max_indices = np.argmax(tempogram, axis=0)
 dominant_tempos = tempos[max_indices]
 
+# plot the dominant tempo vs time
 plt.figure(figsize=(10, 4))
 plt.plot(times, dominant_tempos, label='Dominant Tempo (BPM)')
 plt.xlabel('Time (s)')
@@ -46,8 +58,8 @@ plt.ylabel('Tempo (BPM)')
 plt.title('Tempo (BPM) vs Time (s)')
 plt.show()
 
+# add clicks to the original audio to check
 frame_times = librosa.frames_to_time(np.arange(len(dominant_tempos)), sr=sr, hop_length=hop_length)
-
 beat_periods = 60.0/dominant_tempos 
 
 beat_times = []
@@ -57,6 +69,6 @@ for t0, period in zip(frame_times, beat_periods):
 beat_times = np.unique(np.array(beat_times))
 
 clicks = librosa.clicks(times=beat_times, sr=sr, click_duration=0.03, length=len(signal))
-click_overlay = signal + clicks
+click_overlay = signal + 0.5*clicks
 
 sf.write(f'audio_out/{os.path.splitext(filename)[0]}_clicks.mp3', click_overlay, sr)
